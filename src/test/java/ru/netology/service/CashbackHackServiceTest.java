768b05ef873a11307ebb79f9725627b51248d414
unknown <kenseisan23@gmail.com>
2023-06-15 22:10:53 +0300

    Gitignore added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,36 +1,38 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import пorg.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
-    @Test
-    public void CashbackTest() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1500;
-        int expected = service.remain(amount);
-        int actual = 500;
-        assertEquals(actual, expected);
 
-    }
+        @Test
+        public void CashbackTest() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 999;
+            int expected = 1;
+            int actual = service.remain(amount);
+            assertEquals(expected, actual);
 
-    @Test
-    public void CashBackTest2() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1400;
-        int expected = service.remain(amount);
-        int actual = 600;
-        assertEquals(actual, expected);
-    }
-    @Test
-    public void CashbackTest3() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 500;
-        int expected = service.remain(amount);
-        int actual = 500;
-        assertEquals(actual, expected);
+        }
 
-    }
+        @Test
+        public void CashBackTest2() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 1000;
+            int expected = 0;
+            int actual = service.remain(amount);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void CashbackTest3() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 1001;
+            int expected = 999;
+            int actual = service.remain(amount);
+            assertEquals(expected, actual);
+
+        }
 }
